81ebd889641500320156544afa536dbb86d577e7
Nurazizahra <nurazizahrabiulakhir@gmail.com>
2024-07-01 01:20:36 +0700

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/scripts/index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
+    }),
+    /* new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, 'src/public/'),
+          to: path.join(__dirname, 'dist/'),
+        },
+      ],
+    }), */
+  ],
+};
